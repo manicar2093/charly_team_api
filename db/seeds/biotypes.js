407648ca8456a292c86e967098a2f9c55aac470a
Manuel Carbajal <manicar2093@gmail.com>
2021-07-17 23:22:35 -0500

    feat: biotypes added to seeds files

---
+++ db/seeds/biotypes.js
@@ -0,0 +1,8 @@
+
+exports.seed = function(knex) {
+  return knex('Biotype').insert([
+    {id: 1, description: 'Ectomorph'},
+    {id: 2, description: 'Mesomorph'},
+    {id: 3, description: 'Endomorph'},
+  ]);
+};
