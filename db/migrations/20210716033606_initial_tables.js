8bfcb8a86975c52b0f8681257e9c6a825ffa93a9
Manuel Carbajal <manicar2093@gmail.com>
2021-07-20 00:17:01 -0500

    feat: bone density, role and customer tables added. user table modify

--- db/migrations/20210716033606_initial_tables.js
+++ db/migrations/20210716033606_initial_tables.js
@@ -6,27 +6,57 @@ exports.up = function(knex) {
         t.string('description').notNullable();
     });
 
-    const userSchema = () => knex.schema.createTable("User", t => {
+    const bone_density = () => knex.schema.createTable('BoneDensity', t => {
+        t.increments('id').primary();
+        t.string('description').notNullable();
+    });
+
+    const role = () => knex.schema.createTable('Role', t => {
+        t.increments('id').primary();
+        t.string('description').notNullable().unique();
+        t.timestamps(true);
+    });
+
+    const customerSchema = () => knex.schema.createTable("Customer", t => {
         t.increments('id').primary();
         t.integer('biotype_id').notNullable().references('Biotype.id');
+        t.integer('bone_density_id').notNullable().references('BoneDensity.id');
+        t.integer('rol_id').notNullable().references('Role.id');
         t.string('bone_density').notNullable();
         t.string('name').notNullable();
         t.string('last_name').notNullable();
-        t.string('email').unique();
-        t.string('username').unique().nullable();
-        t.string('password').unique().notNullable();
+        t.string('email').unique().nullable();
         t.date('birthday').notNullable();
         t.timestamps(true);
     });
 
+    const userSchema = () => knex.schema.createTable("User", t => {
+        t.increments('id').primary();
+        t.integer('rol_id').notNullable().references('Role.id');
+        t.string('name').notNullable();
+        t.string('last_name').notNullable();
+        t.string('username').unique().nullable();
+        t.string('password').unique().notNullable();
+        t.timestamps(true);
+    });
+
     return biotypes()
+        .then(bone_density)
+        .then(role)
+        .then(customerSchema)
         .then(userSchema);
 };
 
 exports.down = function(knex) {
-    const userSchema = () => knex.schema.dropTable('User');
     const biotypes = () => knex.schema.dropTable('Biotype');
+    const bone_density = () => knex.schema.dropTable('BoneDensity');
+    const role = () => knex.schema.dropTable('Role');
+    const customerSchema = () => knex.schema.dropTable('Customer');
+    const userSchema = () => knex.schema.dropTable('User');
 
     return userSchema()
-        .then(biotypes);
+        .then(customerSchema)
+        .then(biotypes)
+        .then(bone_density)
+        .then(role);
 };
