63d316bdc5c78404c084027fbfee99c3017d609e
Manuel Carbajal <manicar2093@gmail.com>
2021-07-20 00:23:11 -0500

    feat: delete data before add in biotype seed

--- db/seeds/biotypes.js
+++ db/seeds/biotypes.js
@@ -1,8 +1,10 @@
 
 exports.seed = function(knex) {
-  return knex('Biotype').insert([
-    {id: 1, description: 'Ectomorph'},
-    {id: 2, description: 'Mesomorph'},
-    {id: 3, description: 'Endomorph'},
-  ]);
+  return knex('Biotype').del().then(()=>{
+    return knex('Biotype').insert([
+      {id: 1, description: 'Ectomorph'},
+      {id: 2, description: 'Mesomorph'},
+      {id: 3, description: 'Endomorph'},
+    ]);
+  });
 };
