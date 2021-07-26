fd52bec6091a3b7f828224062807ac645adfe859
Manuel Carbajal <manicar2093@gmail.com>
2021-07-25 19:21:03 -0500

    fix: remove deletion of table's data when seed is runned

--- db/seeds/roles.js
+++ db/seeds/roles.js
@@ -1,12 +1,7 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('Role').del()
-    .then(function () {
-      // Inserts seed entries
       return knex('Role').insert([
         {id: 1, description: 'ADMIN'},
         {id: 2, description: 'CUSTOMER'},
       ]);
-    });
 };
--- db/seeds/weight_clasifications.js
+++ db/seeds/weight_clasifications.js
@@ -1,9 +1,5 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('WeightClasifications').del()
-    .then(function () {
-      // Inserts seed entries
       return knex('WeightClasifications').insert([
         {id: 1, description: 'Underweight'},
         {id: 2, description: 'Normal weight'},
@@ -12,5 +8,4 @@ exports.seed = function(knex) {
         {id: 5, description: 'Obesity 2'},
         {id: 6, description: 'Obesity 3'},
       ]);
-    });
 };
