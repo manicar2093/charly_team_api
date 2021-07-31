9dd7f0a14083b9e41d2f47f977933e3bc9d66f7d
Manuel Carbajal <manicar2093@gmail.com>
2021-07-30 23:56:21 -0500

    feat: add relation to heart_health table

--- db/migrations/20210716040425_biotest_table.js
+++ db/migrations/20210716040425_biotest_table.js
@@ -19,6 +19,7 @@ exports.up = function(knex) {
         t.integer('lower_muscle_density_id').notNullable().references('LowerMuscleDensity.id');
         t.integer('skin_folds_id').notNullable().references('SkinFolds.id');
         t.integer('weight_clasification_id').notNullable().references('WeightClasifications.id');
+        t.integer('heart_health_id').nullable().references('HeartHealths.id');
         t.integer('customer_id').notNullable().references('Customer.id');
         t.decimal('weight').notNullable();
         t.integer('height').notNullable();
@@ -29,7 +30,6 @@ exports.up = function(knex) {
         t.decimal('glucose').nullable();
         t.decimal('resting_heart_rate').nullable();
         t.decimal('maximum_heart_rate').nullable();
-        t.string('heart_health').nullable();
         t.string('observations').nullable();
         t.string('recommendations').nullable();
         t.string('front_picture').nullable();
