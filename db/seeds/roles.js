93596dbf0f264e0d8302d42d43ab0ceb8ebc525b
Manuel Carbajal <manicar2093@gmail.com>
2021-08-16 00:22:58 -0500

    feat: Add new role.

--- db/seeds/roles.js
+++ db/seeds/roles.js
@@ -2,6 +2,7 @@
 exports.seed = function(knex) {
       return knex('Role').insert([
         {id: 1, description: 'ADMIN'},
-        {id: 2, description: 'CUSTOMER'},
+        {id: 2, description: 'COACH'},
+        {id: 3, description: 'CUSTOMER'},
       ]);
 };
