6a02bc8e3af0eef2ee0d22c64b9c4e63b40381f5
Manuel Carbajal <manicar2093@gmail.com>
2021-08-16 21:44:46 -0500

    feat: Add new configuration for knex.
    New test environment was added to allow to run test with
    
    SQlite3 on testing environment

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,35 @@
 require('dotenv').config()
 
 module.exports = {
-  client: 'postgresql',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      tableName: 'schema_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
   },
-  migrations: {
-    tableName: 'schema_migrations',
-    directory: './db/migrations',
-  },
-  seeds: {
-    directory: './db/seeds',
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: "./testing.db",
+    },
+    migrations: {
+      tableName: 'schema_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+    useNullAsDefault: true,
   },
 };
