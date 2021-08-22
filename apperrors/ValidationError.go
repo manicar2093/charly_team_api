100d8c3b1813ec3c5dd80d69818c558395aa440c
Manuel Carbajal <manicar2093@gmail.com>
2021-08-22 18:51:42 -0500

    feat: Add validation service for structs.

---
+++ apperrors/ValidationError.go
@@ -0,0 +1,20 @@
+package apperrors
+
+import "fmt"
+
+// ValidationError represents a validation error formated to
+// send to the front end
+type ValidationError struct {
+	Field      string `json:"tag"`
+	Validation string `json:"validation"`
+}
+
+// ValidationErrors is a slice of ValidationError.
+// It implements error interface
+type ValidationErrors []ValidationError
+
+func (v ValidationErrors) Error() string {
+	errorCount := len(v)
+
+	return fmt.Sprintf("error count: %v", errorCount)
+}
