216e42a41c234a1b42b27136b7147b63baf484e0
Manuel Carbajal <manicar2093@gmail.com>
2021-08-25 19:54:28 -0500

    feat: Add AWSSession testing and action config,

---
+++ connections/AWSSession_test.go
@@ -0,0 +1,18 @@
+package connections
+
+import (
+	"testing"
+
+	"github.com/manicar2093/charly_team_api/testfunc/asserts"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAWSSession(t *testing.T) {
+
+	t.Run("should connect correctly", func(t *testing.T) {
+		defer asserts.ShouldNotPanic(t)
+		got := GetAWSSession()
+		assert.NotNil(t, got, "aws session was not created")
+	})
+
+}
