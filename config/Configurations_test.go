6a0370f314edc4ca3484edc83de3d9e710134d40
Manuel Carbajal <manicar2093@gmail.com>
2021-08-25 19:53:45 -0500

    test: Add new assert functions for testing.

--- config/Configurations_test.go
+++ config/Configurations_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/joho/godotenv"
+	"github.com/manicar2093/charly_team_api/testfunc/asserts"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,11 +20,7 @@ func TestMain(m *testing.M) {
 func TestGetEnvOrPanica(t *testing.T) {
 
 	t.Run("should panic if env variable does not exists", func(t *testing.T) {
-		defer func() {
-			if r := recover(); r == nil {
-				t.Fatal("should panic. env variable does not exists")
-			}
-		}()
+		defer asserts.ShouldPanic(t, "env variable does not exists")
 		GetEnvOrPanic("NOT_EXISTS")
 	})
 }
