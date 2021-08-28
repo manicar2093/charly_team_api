714b8b99e7f1e3b62c358306c008e6ab7b4524a1
Manuel Carbajal <manicar2093@gmail.com>
2021-08-27 19:11:13 -0500

    feat: Add last changes to registrry_user function

--- handlers/registry_new_user/main.go
+++ handlers/registry_new_user/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/manicar2093/charly_team_api/aws"
 	"github.com/manicar2093/charly_team_api/connections"
@@ -11,38 +9,38 @@ import (
 	"github.com/manicar2093/charly_team_api/validators"
 )
 
-var validator = validators.NewStructValidator()
-var db = connections.PostgressConnection()
-var passGen = services.PasswordGenerator{}
-var userService services.UserService
-
 func main() {
-
-	userService = services.NewUserServiceCognito(
-		aws.NewCognitoClient(),
-		db,
-		passGen,
-	)
-
-	lambda.Start(LambdaHandler)
+	lambda.Start(CreateLambdaHandlerWDependencies(
+		connections.PostgressConnection(),
+		services.PasswordGenerator{},
+		validators.NewStructValidator(),
+	))
 }
 
-func LambdaHandler(req models.CreateUserRequest) (models.Response, error) {
-	isValid, response := validators.CheckValidationErrors(validator.Validate(req))
+func CreateLambdaHandlerWDependencies(
+	db connections.Repository,
+	passGen services.PassGen,
+	validator validators.ValidatorService,
+) interface{} {
 
-	if !isValid {
-		return response, nil
+	return func(req models.CreateUserRequest) (models.Response, error) {
+
+		isValid, response := validators.CheckValidationErrors(validator.Validate(req))
+
+		if !isValid {
+			return response, nil
+		}
+
+		userService := services.NewUserServiceCognito(
+			aws.NewCognitoClient(),
+			db,
+			passGen,
+		)
+
+		err := userService.CreateUser(req)
+
+		return models.Response{}, err
+
 	}
-
-	err := userService.CreateUser(req)
-
-	if err != nil {
-		return models.Response{
-			Code:   http.StatusInternalServerError,
-			Status: http.StatusText(http.StatusInternalServerError),
-			Body:   err.Error(),
-		}, err
-	}
-	return models.Response{}, nil
 
 }
