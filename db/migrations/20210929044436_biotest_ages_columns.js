a581cbcfd7cf76b17f397a7564f24b119f43fdc7
Manuel Carbajal <manicar2093@gmail.com>
2021-09-29 21:49:19 -0500

    fix: Change attributes Biotest names

--- db/migrations/20210929044436_biotest_ages_columns.js
+++ db/migrations/20210929044436_biotest_ages_columns.js
@@ -2,13 +2,13 @@
 exports.up = function(knex) {
     return knex.schema.alterTable('Biotest', t => {
         t.integer('chronological_age').notNullable();
-        t.integer('age_on_test').notNullable();
+        t.integer('corporal_age').notNullable();
     });
 };
 
 exports.down = function(knex) {
     return knex.schema.alterTable('Biotest', t => {
         t.dropColumn('chronological_age');
-        t.dropColumn('age_on_test');
+        t.dropColumn('corporal_age');
     });
 };
