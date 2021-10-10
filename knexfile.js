76cbad75bcb4e15b9b4f969b061b2d7a59846cf6
Manuel Carbajal <manicar2093@gmail.com>
2021-10-10 18:14:05 -0500

    feat: Add env variables for Prod migrations

--- knexfile.js
+++ knexfile.js
@@ -35,11 +35,11 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      host: process.env.PROD_DB_HOST,
+      port: process.env.PROD_DB_PORT,
+      database: process.env.PROD_DB_NAME,
+      user: process.env.PROD_DB_USER,
+      password: process.env.PROD_DB_PASSWORD,
     },
     migrations: {
       tableName: 'schema_migrations',
