3d811dc7fa6ea5e543ac45305ed45b6bf2b35277
manicq <63815702+manicar2093@users.noreply.github.com>
2021-10-18 12:25:21 -0500

    feat: Add lambda to retreive user info when login (#109)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Charly Team API
 
+## What to consider when create a new handler
+
+This new handler must call `config.Start()` to avoid problems on DB connection
+
 ## Testing
 
 Just run the make command ```make test```.
@@ -9,3 +13,7 @@ Into the package ```testfunc``` there is a func to start environment for testing
 ## Error handling
 
 You can create a new struct which implements HandableErrors interfaz to be able to build an response.
+
+## Considerations
+
+On Cognito PreTokekenGen must be configurated manually. [More info](https://stackoverflow.com/questions/54530537/serverless-framework-cognito-userpool-pre-token-generator)
---
+++ handlers/cognito_token_gen/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/go-rel/rel"
+	"github.com/go-rel/rel/where"
+	"github.com/manicar2093/charly_team_api/config"
+	"github.com/manicar2093/charly_team_api/db/connections"
+	"github.com/manicar2093/charly_team_api/db/entities"
+)
+
+func main() {
+	config.StartConfig()
+	lambda.Start(
+		CreateLambdaHandlerWDependencies(
+			connections.PostgressConnection(),
+		),
+	)
+}
+
+func CreateLambdaHandlerWDependencies(
+	repo rel.Repository,
+) func(ctx context.Context, event events.CognitoEventUserPoolsPreTokenGen) (events.CognitoEventUserPoolsPreTokenGen, error) {
+
+	return func(ctx context.Context, event events.CognitoEventUserPoolsPreTokenGen) (events.CognitoEventUserPoolsPreTokenGen, error) {
+		var userToSign entities.User
+
+		email := event.Request.UserAttributes["email"]
+
+		err := repo.Find(ctx, &userToSign, where.Eq("email", email))
+		if err != nil {
+			log.Println(err)
+			return event, errors.New("user was not found")
+		}
+
+		myClaims := map[string]string{
+			"name_to_show": CreateNameToShow(userToSign.Name, userToSign.LastName),
+			"avatar_url":   userToSign.AvatarUrl,
+			"uuid":         userToSign.UserUUID,
+		}
+
+		event.Response.ClaimsOverrideDetails.ClaimsToAddOrOverride = myClaims
+
+		return event, nil
+	}
+
+}
+
+// CreateNameToShow will split names to create a full name compose by first name and paternal surename
+func CreateNameToShow(name, lastName string) string {
+	nameSplitted := strings.Split(name, " ")
+	first, _ := nameSplitted[0], ""
+
+	sureNameSplitted := strings.Split(lastName, " ")
+
+	paternal, _ := sureNameSplitted[0], ""
+
+	return fmt.Sprintf("%s %s", first, paternal)
+
+}
