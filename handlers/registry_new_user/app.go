b9665a3228dda890df8485b9709cac39016fb43b
Manuel Carbajal <manicar2093@gmail.com>
2021-10-21 20:45:10 -0500

    fix: Add int validator to create sql.Int null
    - Pass user service to registry-user handler

--- handlers/registry_new_user/app.go
+++ handlers/registry_new_user/app.go
@@ -1,4 +1,4 @@
-package services
+package main
 
 import (
 	"context"
@@ -12,7 +12,8 @@ import (
 	"github.com/manicar2093/charly_team_api/config"
 	"github.com/manicar2093/charly_team_api/db/entities"
 	"github.com/manicar2093/charly_team_api/models"
-	"gopkg.in/guregu/null.v4"
+	"github.com/manicar2093/charly_team_api/services"
+	"github.com/manicar2093/charly_team_api/validators/nullsql"
 )
 
 type RoleType int32
@@ -21,12 +22,6 @@ var (
 	emailAttributeName string = "email"
 	errorGeneratePass         = errors.New("error generating temporary password")
 	errorSavingUser           = errors.New("error saving user into de db")
-)
-
-const (
-	ADMIN RoleType = iota + 1
-	COACH
-	CUSTOMER
 )
 
 type UserService interface {
@@ -39,16 +34,16 @@ type UserService interface {
 // UserServiceCognito is a middleware to Cognito Services. PoolID is taken from config package.
 type UserServiceCognito struct {
 	provider aws.CongitoClient
-	passGen  PassGen
+	passGen  services.PassGen
 	repo     rel.Repository
-	uuidGen  UUIDGenerator
+	uuidGen  services.UUIDGenerator
 }
 
 func NewUserServiceCognito(
 	provider aws.CongitoClient,
-	passGen PassGen,
+	passGen services.PassGen,
 	repo rel.Repository,
-	uuidGen UUIDGenerator,
+	uuidGen services.UUIDGenerator,
 ) UserService {
 	return &UserServiceCognito{
 		provider: provider,
@@ -63,20 +58,20 @@ func (u UserServiceCognito) CreateUser(
 	user *models.CreateUserRequest,
 ) (*entities.User, error) {
 
-	userEntity := entities.User{
-		Name:          user.Name,
-		LastName:      user.LastName,
-		RoleID:        int32(user.RoleID),
-		GenderID:      null.IntFrom(int64(user.GenderID)),
-		UserUUID:      u.uuidGen.New(),
-		Email:         user.Email,
-		Birthday:      user.Birthday,
-		AvatarUrl:     fmt.Sprintf("%s%s.svg", config.AvatarURLSrc, u.uuidGen.New()),
-		BiotypeID:     null.IntFrom(int64(user.BiotypeID)),
-		BoneDensityID: null.IntFrom(int64(user.BoneDensityID)),
-	}
+	var userEntity entities.User
 
 	err := u.repo.Transaction(ctx, func(ctx context.Context) error {
+		userEntity = entities.User{
+			Name:          user.Name,
+			LastName:      user.LastName,
+			RoleID:        int32(user.RoleID),
+			GenderID:      nullsql.ValidateIntSQLValid(int64(user.GenderID)),
+			Email:         user.Email,
+			Birthday:      user.Birthday,
+			AvatarUrl:     fmt.Sprintf("%s%s.svg", config.AvatarURLSrc, u.uuidGen.New()),
+			BiotypeID:     nullsql.ValidateIntSQLValid(int64(user.BiotypeID)),
+			BoneDensityID: nullsql.ValidateIntSQLValid(int64(user.BoneDensityID)),
+		}
 
 		pass, err := u.passGen.Generate()
 		if err != nil {
@@ -96,6 +91,15 @@ func (u UserServiceCognito) CreateUser(
 			},
 		}
 
+		userOutput, err := u.provider.AdminCreateUser(&requestData)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		userEntity.UserUUID = *userOutput.User.Username
+		log.Println(userEntity)
+
 		err = u.repo.Insert(ctx, &userEntity)
 
 		if err != nil {
@@ -103,15 +107,11 @@ func (u UserServiceCognito) CreateUser(
 			return errorSavingUser
 		}
 
-		_, err = u.provider.AdminCreateUser(&requestData)
-		if err != nil {
-			return err
-		}
-
 		return nil
 	})
 
 	if err != nil {
+		log.Println(err)
 		return &entities.User{}, err
 	}
 
