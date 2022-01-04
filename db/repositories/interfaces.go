7d8402da3c29e9a0bcf8ee54b5fed8ba51b3e74a
Manuel Carbajal <manicar2093@gmail.com>
2022-01-03 21:07:46 -0600

    feat: Add BiotestRepository interface

---
+++ db/repositories/interfaces.go
@@ -0,0 +1,15 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/manicar2093/charly_team_api/db/entities"
+	"github.com/manicar2093/charly_team_api/db/paginator"
+)
+
+type BiotestRepository interface {
+	FindBiotestByUUID(ctx context.Context, biotestUUID string) (*entities.Biotest, error)
+	GetAllUserBiotestByUserUUID(ctx context.Context, userUUID string) (*paginator.Paginator, error)
+	GetAllUserBiotestByUserUUIDAsCatalog(ctx context.Context, userUUID string) (*paginator.Paginator, error)
+	GetComparitionDataByUserUUID(ctx context.Context, userUUID string) (*BiotestComparisionResponse, error)
+}
