33dd8f2c6720e7b689e608b94b8f7ed8fcda5d43
Manuel Carbajal <manicar2093@gmail.com>
2022-01-03 18:32:11 -0600

    feat: Add PageSize attribute on Paginator model

--- models/Paginator.go
+++ models/Paginator.go
@@ -4,6 +4,7 @@ type Paginator struct {
 	TotalPages   int         `json:"total_pages,omitempty"`
 	CurrentPage  int         `json:"current_page,omitempty"`
 	PreviousPage int         `json:"previous_page,omitempty"`
+	PageSize     int         `json:"page_size,omitempty"`
 	NextPage     int         `json:"next_page,omitempty"`
 	TotalEntries int         `json:"total_entries,omitempty"`
 	Data         interface{} `json:"data,omitempty"`
