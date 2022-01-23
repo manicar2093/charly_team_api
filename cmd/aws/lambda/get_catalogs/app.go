f00511cfa93fb7ea7c7151c0344f8df06c5a32e1
Manuel Carbajal <manicar2093@gmail.com>
2022-01-23 15:21:28 -0600

    feat: Add get catalogs aws lambda

---
+++ cmd/aws/lambda/get_catalogs/app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/manicar2093/charly_team_api/handlers/cataloggetter"
+	"github.com/manicar2093/charly_team_api/models"
+)
+
+type GetCatalogsAWSLambda struct {
+	catalogsGetter cataloggetter.CatalogGetter
+}
+
+func NewGetCatalogsAWSLambda(catalogsGetter cataloggetter.CatalogGetter) *GetCatalogsAWSLambda {
+	return &GetCatalogsAWSLambda{catalogsGetter: catalogsGetter}
+}
+
+func (c *GetCatalogsAWSLambda) Handler(ctx context.Context, catalogs cataloggetter.CatalogGetterRequest) (*models.Response, error) {
+	res, err := c.catalogsGetter.Run(ctx, &catalogs)
+
+	if err != nil {
+		return models.CreateResponseFromError(err), nil
+	}
+
+	return models.CreateResponse(
+		http.StatusOK,
+		res.Catalogs,
+	), nil
+}
