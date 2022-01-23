262585dfe90a8bb0b244972efc70a37798019881
Manuel Carbajal <manicar2093@gmail.com>
2022-01-22 20:30:44 -0600

    feat: Add impl to save users

---
+++ handlers/usercreator/models.go
@@ -0,0 +1,48 @@
+package usercreator
+
+import (
+	"errors"
+	"time"
+
+	"github.com/manicar2093/charly_team_api/db/entities"
+)
+
+const (
+	AdminRole = iota + 1
+	CoachRole
+	CustomerRole
+)
+
+var (
+	emailAttributeName  string = "email"
+	generationPassError        = errors.New("error generating temporary password")
+	savingUserDBError          = errors.New("error saving user into db")
+	savingUserAWSError         = errors.New("error saving user into cognito")
+)
+
+type UserCreatorRequest struct {
+	Name          string    `json:"name,omitempty" validate:"required"`
+	LastName      string    `json:"last_name,omitempty" validate:"required"`
+	Email         string    `json:"email,omitempty" validate:"required,email"`
+	Birthday      time.Time `json:"birthday,omitempty" validate:"required"`
+	RoleID        int       `json:"role_id,omitempty" validate:"required,gt=0"`
+	GenderID      int       `json:"gender_id,omitempty"`
+	BoneDensityID int       `json:"bone_density_id,omitempty"`
+	BiotypeID     int       `json:"biotype_id,omitempty"`
+}
+
+func (c *UserCreatorRequest) GetCustomerCreationValidations() interface{} {
+	return struct {
+		GenderID      int `validate:"required,gt=0" json:"gender_id,omitempty"`
+		BoneDensityID int `validate:"required,gt=0" json:"bone_density_id,omitempty"`
+		BiotypeID     int `validate:"required,gt=0" json:"biotype_id,omitempty"`
+	}{
+		GenderID:      c.GenderID,
+		BoneDensityID: c.BoneDensityID,
+		BiotypeID:     c.BiotypeID,
+	}
+}
+
+type UserCreatorResponse struct {
+	UserCreated *entities.User
+}
