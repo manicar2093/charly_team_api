cce4dc583793f2bb93f0883fea2ec1541c181175
Manuel Carbajal <manicar2093@gmail.com>
2022-01-23 18:31:20 -0600

    feat: Add biotest images saver  aws lambda impl

---
+++ cmd/aws/lambda/save_biotest_images/README.md
@@ -0,0 +1,24 @@
+# Save biotest images
+
+## Request
+
+```json
+{
+    "biotest_uuid": "biotest_uuid",
+    "front_picture": "front/image/path",
+    "back_picture": "back/image/path",
+    "right_side_picture": "right/image/path",
+    "left_side_picture": "left/image/path"
+}
+```
+
+biotest_uuid is required
+
+## Response
+
+```json
+{
+    "code": 200,
+    "status": "OK"
+}
+```
---
+++ cmd/aws/lambda/save_biotest_images/app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/manicar2093/charly_team_api/handlers/biotestimagessaver"
+	"github.com/manicar2093/charly_team_api/models"
+)
+
+type SaveBiotestImagesAWSLambda struct {
+	biotestImageSaver biotestimagessaver.BiotestImagesSaver
+}
+
+func NewSaveBiotestImagesAWSLambda(biotestImageSaver biotestimagessaver.BiotestImagesSaver) *SaveBiotestImagesAWSLambda {
+	return &SaveBiotestImagesAWSLambda{biotestImageSaver: biotestImageSaver}
+}
+
+func (c *SaveBiotestImagesAWSLambda) Handler(ctx context.Context, biotestImages biotestimagessaver.BiotestImagesSaverRequest) (*models.Response, error) {
+	_, err := c.biotestImageSaver.Run(ctx, &biotestImages)
+
+	if err != nil {
+		return models.CreateResponseFromError(err), nil
+	}
+
+	return models.CreateResponse(
+		http.StatusOK,
+		nil,
+	), nil
+}
