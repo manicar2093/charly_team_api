334df31bd4d5ff91daa549e4da7b4579c3a5cd6c
Manuel Carbajal <manicar2093@gmail.com>
2022-01-24 15:53:38 -0600

    feat: Add find biotest by uuid aws lambda controller

---
+++ cmd/aws/lambda/find_biotest_by_uuid/app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/manicar2093/charly_team_api/handlers/biotestfilters/biotestbyuuidfinder"
+	"github.com/manicar2093/charly_team_api/models"
+)
+
+type FindBiotestByUUIDAWSLambda struct {
+	biotestByUUIDFinder biotestbyuuidfinder.BiotestByUUID
+}
+
+func NewFindBiotestByUUIDAWSLambda(biotestByUUIDFinder biotestbyuuidfinder.BiotestByUUID) *FindBiotestByUUIDAWSLambda {
+	return &FindBiotestByUUIDAWSLambda{biotestByUUIDFinder: biotestByUUIDFinder}
+}
+
+func (c *FindBiotestByUUIDAWSLambda) Handler(ctx context.Context, req biotestbyuuidfinder.BiotestByUUIDRequest) (*models.Response, error) {
+	res, err := c.biotestByUUIDFinder.Run(ctx, &req)
+
+	if err != nil {
+		return models.CreateResponseFromError(err), nil
+	}
+
+	return models.CreateResponse(
+		http.StatusOK,
+		res.Biotest,
+	), nil
+}
