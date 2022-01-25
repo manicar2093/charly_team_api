57e4835f05f75f6fe8f7d006bde59463b29a6fae
Manuel Carbajal <manicar2093@gmail.com>
2022-01-25 14:45:42 -0600

    feat: Add biotest data comparition aws lambda

---
+++ cmd/aws/lambda/get_biotest_comparition_data/app.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/manicar2093/charly_team_api/handlers/biotestfilters/biotestcomparitiondatafinder"
+	"github.com/manicar2093/charly_team_api/models"
+)
+
+type GetBiotestComparitionDataAWSLambda struct {
+	biotestComparitionDataFinder biotestcomparitiondatafinder.BiotestComparitionDataFinder
+}
+
+func NewGetBiotestComparitionDataAWSLambda(
+	biotestComparitionDataFinder biotestcomparitiondatafinder.BiotestComparitionDataFinder,
+) *GetBiotestComparitionDataAWSLambda {
+	return &GetBiotestComparitionDataAWSLambda{
+		biotestComparitionDataFinder: biotestComparitionDataFinder,
+	}
+}
+
+func (c *GetBiotestComparitionDataAWSLambda) Handler(
+	ctx context.Context,
+	req biotestcomparitiondatafinder.BiotestComparitionDataFinderRequest,
+) (*models.Response, error) {
+	res, err := c.biotestComparitionDataFinder.Run(ctx, &req)
+
+	if err != nil {
+		return models.CreateResponseFromError(err), nil
+	}
+
+	return models.CreateResponse(
+		http.StatusOK,
+		res.ComparitionData,
+	), nil
+}
