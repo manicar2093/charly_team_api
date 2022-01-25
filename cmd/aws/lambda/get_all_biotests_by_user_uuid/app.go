7983819d8eed3a7b1e22dfe86337585375d3b21b
Manuel Carbajal <manicar2093@gmail.com>
2022-01-24 18:37:27 -0600

    feat: Add all biotest by user uuid impl

---
+++ cmd/aws/lambda/get_all_biotests_by_user_uuid/app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/manicar2093/charly_team_api/handlers/biotestfilters/biotestsbyuseruuidfinder"
+	"github.com/manicar2093/charly_team_api/models"
+)
+
+type GetAllBiotestByUserUUIDAWSLambda struct {
+	biotestsByUserUUIDFinder biotestsbyuseruuidfinder.BiotestByUserUUID
+}
+
+func NewGetAllBiotestByUserUUIDAWSLambda(biotestsByUserUUIDFinder biotestsbyuseruuidfinder.BiotestByUserUUID) *GetAllBiotestByUserUUIDAWSLambda {
+	return &GetAllBiotestByUserUUIDAWSLambda{biotestsByUserUUIDFinder: biotestsByUserUUIDFinder}
+}
+
+func (c *GetAllBiotestByUserUUIDAWSLambda) Handler(ctx context.Context, req biotestsbyuseruuidfinder.BiotestByUserUUIDRequest) (*models.Response, error) {
+	res, err := c.biotestsByUserUUIDFinder.Run(ctx, &req)
+
+	if err != nil {
+		return models.CreateResponseFromError(err), nil
+	}
+
+	return models.CreateResponse(
+		http.StatusOK,
+		res.FoundBiotests,
+	), nil
+}
