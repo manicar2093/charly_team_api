6a8075d36a015dc078864e8324434d80ee612a4e
Manuel Carbajal <manicar2093@gmail.com>
2022-01-25 14:45:57 -0600

    test: Add test for aws lambda controller

---
+++ cmd/aws/lambda/get_biotest_comparition_data/app_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/jaswdr/faker"
+	"github.com/manicar2093/charly_team_api/db/entities"
+	"github.com/manicar2093/charly_team_api/db/repositories"
+	"github.com/manicar2093/charly_team_api/handlers/biotestfilters/biotestcomparitiondatafinder"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestMain(t *testing.T) {
+	suite.Run(t, new(GetBiotestComparitionDataAWSLambdaTests))
+}
+
+type GetBiotestComparitionDataAWSLambdaTests struct {
+	suite.Suite
+	ctx                                context.Context
+	biotestComparitionDataFinder       *biotestcomparitiondatafinder.MockBiotestComparitionDataFinder
+	getBiotestComparitionDataAWSLambda *GetBiotestComparitionDataAWSLambda
+	faker                              faker.Faker
+}
+
+func (c *GetBiotestComparitionDataAWSLambdaTests) SetupTest() {
+	c.ctx = context.Background()
+	c.biotestComparitionDataFinder = &biotestcomparitiondatafinder.MockBiotestComparitionDataFinder{}
+	c.getBiotestComparitionDataAWSLambda = NewGetBiotestComparitionDataAWSLambda(c.biotestComparitionDataFinder)
+	c.faker = faker.New()
+}
+
+func (c *GetBiotestComparitionDataAWSLambdaTests) TearDownTest() {
+	c.biotestComparitionDataFinder.AssertExpectations(c.T())
+}
+
+func (c *GetBiotestComparitionDataAWSLambdaTests) TestHandler() {
+	userUUID := c.faker.UUID().V4()
+	request := biotestcomparitiondatafinder.BiotestComparitionDataFinderRequest{UserUUID: userUUID}
+	biotestComparitionDataReturn := biotestcomparitiondatafinder.BiotestComparitionDataFinderResponse{ComparitionData: &repositories.BiotestComparisionResponse{FirstBiotest: &entities.Biotest{}, LastBiotest: &entities.Biotest{}, AllBiotestsDetails: &[]repositories.BiotestDetails{}}}
+	c.biotestComparitionDataFinder.On("Run", c.ctx, &request).Return(
+		&biotestComparitionDataReturn,
+		nil,
+	)
+
+	got, err := c.getBiotestComparitionDataAWSLambda.Handler(c.ctx, request)
+
+	c.Nil(err)
+	c.NotNil(got)
+}
+
+func (c *GetBiotestComparitionDataAWSLambdaTests) TestHandler_UnhandledError() {
+	userUUID := c.faker.UUID().V4()
+	request := biotestcomparitiondatafinder.BiotestComparitionDataFinderRequest{UserUUID: userUUID}
+	ordinaryError := fmt.Errorf("ordinary error")
+	c.biotestComparitionDataFinder.On("Run", c.ctx, &request).Return(
+		nil,
+		ordinaryError,
+	)
+
+	got, err := c.getBiotestComparitionDataAWSLambda.Handler(c.ctx, request)
+
+	c.Nil(err)
+	c.NotNil(got)
+}
