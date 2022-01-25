c876df6613921d5c4a020f24c3ca55410afb0af0
Manuel Carbajal <manicar2093@gmail.com>
2022-01-24 19:34:55 -0600

    feat: Add biotest comparition data finder impl

---
+++ handlers/biotestfilters/biotestcomparitiondatafinder/models.go
@@ -0,0 +1,13 @@
+package biotestcomparitiondatafinder
+
+import (
+	"github.com/manicar2093/charly_team_api/db/repositories"
+)
+
+type BiotestComparitionDataFinderRequest struct {
+	UserUUID string `validate:"required" json:"user_uuid"`
+}
+
+type BiotestComparitionDataFinderResponse struct {
+	ComparitionData *repositories.BiotestComparisionResponse
+}
