4224173fe3197dc98c2b871e801d0351b9edfa9f
manicq <63815702+manicar2093@users.noreply.github.com>
2022-01-25 20:58:52 -0600

    feat: add user by uuid aws lambda (#162)
    * feat: Add user by uuid finder impl
    
    * test: Add test for impl
    
    * test: Add mock for package
    
    * docs: Add README.md
    
    * feat: add user by uuid aws lambda controller
    
    * test: Add test for new impl
    
    * docs: Add README.md

---
+++ cmd/aws/lambda/get_user_by_uuid/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/manicar2093/charly_team_api/config"
+	"github.com/manicar2093/charly_team_api/db/connections"
+	"github.com/manicar2093/charly_team_api/db/paginator"
+	"github.com/manicar2093/charly_team_api/db/repositories"
+	"github.com/manicar2093/charly_team_api/handlers/userfilters/userbyuuidfinder"
+	"github.com/manicar2093/charly_team_api/validators"
+)
+
+func main() {
+	config.StartConfig()
+	conn := connections.PostgressConnection()
+	paginator := paginator.NewPaginable(conn)
+	userRepo := repositories.NewUserRepositoryRel(
+		conn,
+		paginator,
+	)
+	service := userbyuuidfinder.NewUserByUUIDFinderImpl(
+		userRepo,
+		validators.NewStructValidator(),
+	)
+	lambda.Start(
+		NewGetUserByUUIDAWSLambda(service).Handler,
+	)
+}
---
+++ handlers/userfilters/userbyuuidfinder/models.go
@@ -0,0 +1,11 @@
+package userbyuuidfinder
+
+import "github.com/manicar2093/charly_team_api/db/entities"
+
+type UserByUUIDFinderRequest struct {
+	UserUUID string `validate:"required" json:"user_uuid"`
+}
+
+type UserByUUIDFinderResponse struct {
+	UserFound *entities.User
+}
