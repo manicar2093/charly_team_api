8687325d40ebd78345d967d6eaad86fce1637a43
Manuel Carbajal <manicar2093@gmail.com>
2022-01-26 18:02:24 -0600

    feat: Add get all users aws lambda controller

---
+++ cmd/aws/lambda/get_all_users/app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/manicar2093/charly_team_api/handlers/userfilters/allusersfinder"
+	"github.com/manicar2093/charly_team_api/models"
+)
+
+type GetAllUsersAWSLambda struct {
+	allUsersFinder allusersfinder.AllUsersFinder
+}
+
+func NewGetAllUsersAWSLambda(allUsersFinder allusersfinder.AllUsersFinder) *GetAllUsersAWSLambda {
+	return &GetAllUsersAWSLambda{allUsersFinder: allUsersFinder}
+}
+
+func (c *GetAllUsersAWSLambda) Handler(
+	ctx context.Context,
+	req allusersfinder.AllUsersFinderRequest,
+) (*models.Response, error) {
+	res, err := c.allUsersFinder.Run(ctx, &req)
+
+	if err != nil {
+		return models.CreateResponseFromError(err), nil
+	}
+
+	return models.CreateResponse(
+		http.StatusOK,
+		res.UsersFound,
+	), nil
+}
---
+++ cmd/aws/lambda/get_all_users/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/manicar2093/charly_team_api/config"
+	"github.com/manicar2093/charly_team_api/db/connections"
+	"github.com/manicar2093/charly_team_api/db/paginator"
+	"github.com/manicar2093/charly_team_api/db/repositories"
+	"github.com/manicar2093/charly_team_api/handlers/userfilters/allusersfinder"
+	"github.com/manicar2093/charly_team_api/validators"
+)
+
+func main() {
+	config.StartConfig()
+
+	conn := connections.PostgressConnection()
+	paginator := paginator.NewPaginable(conn)
+	userRepo := repositories.NewUserRepositoryRel(conn, paginator)
+	service := allusersfinder.NewAllUsersFinderImpl(
+		userRepo,
+		validators.NewStructValidator(),
+	)
+	lambda.Start(
+		NewGetAllUsersAWSLambda(service).Handler,
+	)
+}
