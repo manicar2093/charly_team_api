b234a3bd741ea518f98d95bb9546c48c29bb9acc
Manuel Carbajal <manicar2093@gmail.com>
2022-01-29 12:18:07 -0600

    feat: Change items order on catalog seed

--- db/seeds/heart_healths.js
+++ db/seeds/heart_healths.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex) {
     return knex('HeartHealths').insert([
-        {id: 1, description: 'GOOD'},
-        {id: 2, description: 'BAD'},
+        {id: 1, description: 'BAD'},
+        {id: 2, description: 'GOOD'},
         {id: 3, description: 'EXCELLENT'},
     ]);
 };
