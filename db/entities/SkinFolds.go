517c38502975e9378a65f91589abbae4bc7cbaae
Manuel Carbajal <manicar2093@gmail.com>
2022-01-30 18:24:42 -0600

    feat: Modify entities to fit db schema

--- db/entities/SkinFolds.go
+++ db/entities/SkinFolds.go
@@ -5,6 +5,8 @@ import "gopkg.in/guregu/null.v4"
 // SkinFolds is an info wrapper
 type SkinFolds struct {
 	ID          int32    `db:",primary" json:"id,omitempty"`
+	Biotest     *Biotest `json:"biotest,omitempty"`
+	BiotestID   *int32   `json:"biotest_id,omitempty"`
 	Subscapular null.Int `json:"subscapular,omitempty"`
 	Suprailiac  null.Int `json:"suprailiac,omitempty"`
 	Bicipital   null.Int `json:"bicipital,omitempty"`
