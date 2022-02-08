b7d9889c62a12cad0b6cdb74dee14aae3127846b
manicq <63815702+manicar2093@users.noreply.github.com>
2022-02-07 21:06:49 -0600

    fix: Add logger and config start (#179)

--- cmd/aws/lambda/update_user_avatar/main.go
+++ cmd/aws/lambda/update_user_avatar/main.go
@@ -5,11 +5,13 @@ import (
 	"github.com/manicar2093/charly_team_api/db/connections"
 	"github.com/manicar2093/charly_team_api/db/paginator"
 	"github.com/manicar2093/charly_team_api/db/repositories"
+	"github.com/manicar2093/charly_team_api/internal/config"
 	"github.com/manicar2093/charly_team_api/internal/handlers/user"
 	"github.com/manicar2093/charly_team_api/internal/validators"
 )
 
 func main() {
+	config.StartConfig()
 	repo := connections.PostgressConnection()
 	paginator := paginator.NewPaginable(repo)
 	userRepo := repositories.NewUserRepositoryRel(repo, paginator)
