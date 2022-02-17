9561a7902caebd41555bee1178e0575055e15e0c
Manuel Carbajal <manicar2093@gmail.com>
2022-02-16 18:36:29 -0600

    feat: Add new connection to knex and use it on Makefile

--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,13 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      tableName: 'schema_migrations',
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
+  client: 'postgresql',
+  connection: process.env.DB_URL,
+  migrations: {
+    tableName: 'schema_migrations',
+    directory: './db/migrations',
   },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: "./testing.db",
-    },
-    migrations: {
-      tableName: 'schema_migrations',
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
-    useNullAsDefault: true,
-  },
-  production: {
-    client: 'postgresql',
-    connection: {
-      host: process.env.PROD_DB_HOST,
-      port: process.env.PROD_DB_PORT,
-      database: process.env.PROD_DB_NAME,
-      user: process.env.PROD_DB_USER,
-      password: process.env.PROD_DB_PASSWORD,
-    },
-    migrations: {
-      tableName: 'schema_migrations',
-      directory: './db/migrations',
-    },
-    seeds: {
-      directory: './db/seeds',
-    },
+  seeds: {
+    directory: './db/seeds',
   },
 };
