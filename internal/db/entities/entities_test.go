1b60f68335dc7ac20c80750b17b81cc7bc176418
manicq <63815702+manicar2093@users.noreply.github.com>
2023-09-02 01:31:30 -0600

    fix: github-actions (#200)
    * fix: github-actions
    
    * chore: remove process env log
    
    * fix: change test makefile command
    
    * fix: use makefile test command in action

--- internal/db/entities/entities_test.go
+++ internal/db/entities/entities_test.go
@@ -6,13 +6,12 @@ import (
 
 	"github.com/go-rel/rel"
 	"github.com/manicar2093/health_records/internal/db/connections"
-	"github.com/manicar2093/health_records/pkg/testfunc"
 )
 
 var DB rel.Repository
 
 func TestMain(m *testing.M) {
-	testfunc.LoadEnvFileOrPanic("../../../.env")
+	// testfunc.LoadEnvFileOrPanic("../../../.env")
 	db := connections.PostgressConnection()
 	DB = db
 	os.Exit(m.Run())
